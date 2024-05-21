5bca249064124321a634829a3823cccb6745b587
Md. Monir Hoshen <getmonirr@gmail.com>
2024-05-22 00:07:00 +0600

    Feature: initial setup with antd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import AllProviders from "@/providers/All_Providers";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AntdRegistry>
+          <AllProviders>{children}</AllProviders>
+        </AntdRegistry>
+      </body>
     </html>
   );
 }
